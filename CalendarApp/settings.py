6aa22601a01cd7f4a78cf475fe7646ac336bcb79
Luka Goguadze <lukagoguadze2@gmail.com>
2024-12-11 13:36:58 +0400

    migrate to postgres

--- CalendarApp/settings.py
+++ CalendarApp/settings.py
@@ -94,8 +94,12 @@ WSGI_APPLICATION = 'CalendarApp.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get('DB_NAME'),
+        'USER': os.environ.get('DB_USER'),
+        'PASSWORD': os.environ.get('DB_PASSWORD'),
+        'HOST': os.environ.get('DB_HOST'),
+        'PORT': os.environ.get('DB_PORT'),
     }
 }
 
