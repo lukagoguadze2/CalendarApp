7d3354fbf49482c5e4aa37960bf4c5ace54c0a2b
Luka Goguadze <lukagoguadze2@gmail.com>
2024-12-11 16:24:04 +0400

    added models for universities app

--- universities/models.py
+++ universities/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+
+class University(models.Model):
+    name = models.CharField(max_length=255)
+
+
+class Faculty(models.Model):
+    name = models.CharField(max_length=255)
+    university = models.ForeignKey(University, on_delete=models.CASCADE)
+
+
+class Course(models.Model):
+    name = models.CharField(max_length=255)
+    faculty = models.ForeignKey(Faculty, on_delete=models.CASCADE)
+    duration_in_weeks = models.PositiveIntegerField()
