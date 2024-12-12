373d81f1401313ad5f09c233b8928eac77744168
Luka Goguadze <lukagoguadze2@gmail.com>
2024-12-12 14:57:09 +0400

    added handler404

--- CalendarApp/urls.py
+++ CalendarApp/urls.py
@@ -17,11 +17,15 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
+from django.views.generic import TemplateView
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('auth/', include('rest_framework.urls')),
 ]
+
+handler404 = TemplateView.as_view(template_name='404.html')
 
 if settings.DEBUG:
     from debug_toolbar.toolbar import debug_toolbar_urls
